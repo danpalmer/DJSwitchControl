58b12c539edf98a21dcaf0b1ea156d6d35362613
Dan Palmer <dan.palmer@me.com>
2011-12-22 15:06:51 +0000

    Initial Commit

---
+++ DJSwitchControl/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  DJSwitchControl
+//
+//  Created by Dan Palmer on 22/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ DJSwitchControl/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DJSwitchControl
+//
+//  Created by Dan Palmer on 22/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
